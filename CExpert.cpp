9895d89be7014378d1335a6ebf00cceefeaacb86
Vidit <viditrajpurohit27@gmail.com>
2023-07-23 07:49:33 +0530

    Initial commit

---
+++ CExpert.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Enter the number you want the table : ";
+   int a;
+   cin>>a;
+   for(int i=1;i<11;i++){
+      cout<<a<<" x "<<i<<" = "<<a*i<<endl;
+   }
+}
+  
+
+
+
